972b48943062bc4086a44992aa29c7c7d01c1147
Christine Hsieh(謝子涔) <Christine_Hsieh@asus.com>
2024-11-22 16:31:45 +0800

    refactor: remove code redundancy

--- main.py
+++ main.py
@@ -8,30 +8,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import timm
-import tome
-from tqdm import tqdm
 from datetime import datetime
 import time
 import sys
+
 # from utils import set_seed, write_config_log, write_result_log
 import os
 import wandb
-from torchinfo import summary
 import yaml
 
-os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 # CONFIG
 # Learning Options
-EPOCHS     = 50           # train how many epochs
-BATCH_SIZE = 128          # batch size for dataloader (256太大了)
-USE_ADAM   = False        # Adam or SGD optimizer
-LR         = 1e-3         # learning rate
-MILESTONES = [16, 32, 45] # reduce learning rate at 'milestones' epochs
+EPOCHS = 50  # train how many epochs
+BATCH_SIZE = 128  # batch size for dataloader (256太大了)
+USE_ADAM = False  # Adam or SGD optimizer
+LR = 1e-3  # learning rate
+MILESTONES = [16, 32, 45]  # reduce learning rate at 'milestones' epochs
 
 # Param
 
+
 class ModelConfig:
-    def __init__(self, exp_name: str, model_type: str, model_name: str, runs: int, epochs: int, batch_size: int, learning_rate: float):
+    def __init__(
+        self,
+        exp_name: str,
+        model_type: str,
+        model_name: str,
+        runs: int,
+        epochs: int,
+        batch_size: int,
+        learning_rate: float,
+    ):
         self.exp_name = exp_name
         self.model_type = model_type
         self.model_name = model_name
@@ -40,64 +48,91 @@ class ModelConfig:
         self.batch_size = batch_size
         self.learning_rate = learning_rate
 
-def write_result_log(logfile_path, epoch, epoch_time, train_acc, val_acc, train_loss, val_loss, is_better):
-    ''' write experiment log file for result of each epoch to ./experiment/{exp_name}/log/result_log.txt '''
-    with open(logfile_path, 'a') as f:
-        f.write(f'[{epoch + 1}/{cfg.epochs}] {epoch_time:.2f} sec(s) Train Acc: {train_acc:.5f} | Val Acc: {val_acc:.5f} | Train Loss: {train_loss:.5f} | Val Loss: {val_loss:.5f}')
+
+def write_result_log(
+    logfile_path, epoch, epoch_time, train_acc, val_acc, train_loss, val_loss, is_better
+):
+    """write experiment log file for result of each epoch to ./experiment/{exp_name}/log/result_log.txt"""
+    with open(logfile_path, "a") as f:
+        f.write(
+            f"[{epoch + 1}/{cfg.epochs}] {epoch_time:.2f} sec(s) Train Acc: {train_acc:.5f} | Val Acc: {val_acc:.5f} | Train Loss: {train_loss:.5f} | Val Loss: {val_loss:.5f}"
+        )
         if is_better:
-            f.write(' -> val best (acc)')
-        f.write('\n')
+            f.write(" -> val best (acc)")
+        f.write("\n")
+
 
 def plot_learning_curve(logfile_dir, result_lists):
     # 確保資料轉換為 CPU 再繪圖
-    train_acc = [x.cpu().numpy() if isinstance(x, torch.Tensor) else x for x in result_lists['train_acc']]
-    train_loss = [x.cpu().numpy() if isinstance(x, torch.Tensor) else x for x in result_lists['train_loss']]
-    val_acc = [x.cpu().numpy() if isinstance(x, torch.Tensor) else x for x in result_lists['val_acc']]
-    val_loss = [x.cpu().numpy() if isinstance(x, torch.Tensor) else x for x in result_lists['val_loss']]
+    train_acc = [
+        x.cpu().numpy() if isinstance(x, torch.Tensor) else x
+        for x in result_lists["train_acc"]
+    ]
+    train_loss = [
+        x.cpu().numpy() if isinstance(x, torch.Tensor) else x
+        for x in result_lists["train_loss"]
+    ]
+    val_acc = [
+        x.cpu().numpy() if isinstance(x, torch.Tensor) else x
+        for x in result_lists["val_acc"]
+    ]
+    val_loss = [
+        x.cpu().numpy() if isinstance(x, torch.Tensor) else x
+        for x in result_lists["val_loss"]
+    ]
 
-    
     plt.figure()
     plt.plot(train_acc)
-    plt.title('Train Accuracy History')
-    plt.ylabel('Train Accuracy')
-    plt.xlabel('Epoch')
-    plt.legend(['Train accuracy'], loc='lower right')
+    plt.title("Train Accuracy History")
+    plt.ylabel("Train Accuracy")
+    plt.xlabel("Epoch")
+    plt.legend(["Train accuracy"], loc="lower right")
     # plt.show()
-    plt.savefig(os.path.join(logfile_dir, 'train_acc.jpg'))
+    plt.savefig(os.path.join(logfile_dir, "train_acc.jpg"))
     plt.close()
 
     plt.figure()
     plt.plot(train_loss)
-    plt.title('Train Loss History')
-    plt.ylabel('Train Loss')
-    plt.xlabel('Epoch')
-    plt.legend(['Train Loss'], loc='lower right')
+    plt.title("Train Loss History")
+    plt.ylabel("Train Loss")
+    plt.xlabel("Epoch")
+    plt.legend(["Train Loss"], loc="lower right")
     # plt.show()
-    plt.savefig(os.path.join(logfile_dir, 'train_loss.jpg'))
+    plt.savefig(os.path.join(logfile_dir, "train_loss.jpg"))
     plt.close()
 
     plt.figure()
     plt.plot(val_acc)
-    plt.title('Validation Accuracy History')
-    plt.ylabel('Validation Accuracy')
-    plt.xlabel('Epoch')
-    plt.legend(['Valid accuracy'], loc='lower right')
+    plt.title("Validation Accuracy History")
+    plt.ylabel("Validation Accuracy")
+    plt.xlabel("Epoch")
+    plt.legend(["Valid accuracy"], loc="lower right")
     # plt.show()
-    plt.savefig(os.path.join(logfile_dir, 'val_acc.jpg'))
+    plt.savefig(os.path.join(logfile_dir, "val_acc.jpg"))
     plt.close()
 
     plt.figure()
     plt.plot(val_loss)
-    plt.title('Validation Loss History')
-    plt.ylabel('Valid Loss')
-    plt.xlabel('Epoch')
-    plt.legend(['Valid Loss'], loc='lower right')
+    plt.title("Validation Loss History")
+    plt.ylabel("Valid Loss")
+    plt.xlabel("Epoch")
+    plt.legend(["Valid Loss"], loc="lower right")
     # plt.show()
-    plt.savefig(os.path.join(logfile_dir, 'val_loss.jpg'))
+    plt.savefig(os.path.join(logfile_dir, "val_loss.jpg"))
     plt.close()
 
 
-def train_model(device, model, train_loader, val_loader, optimizer, criterion, epochs, model_save_dir, logfile_dir):
+def train_model(
+    device,
+    model,
+    train_loader,
+    val_loader,
+    optimizer,
+    criterion,
+    epochs,
+    model_save_dir,
+    logfile_dir,
+):
     # model.train()  # 設置模型為訓練模式
     train_loss_list, val_loss_list = [], []
     train_acc_list, val_acc_list = [], []
@@ -115,11 +150,15 @@ def train_model(device, model, train_loader, val_loader, optimizer, criterion, e
         train_correct = 0.0
         model.train()
         for batch, data in enumerate(train_loader):
-            sys.stdout.write(f'\r[{epoch + 1}/{epochs}] Train batch: {batch + 1} / {len(train_loader)}')
+            sys.stdout.write(
+                f"\r[{epoch + 1}/{epochs}] Train batch: {batch + 1} / {len(train_loader)}"
+            )
             sys.stdout.flush()
             # Data loading.
             images, labels = data
-            images, labels = images.to(device), labels.to(device) # (batch_size, 3, 32, 32), (batch_size)
+            images, labels = images.to(device), labels.to(
+                device
+            )  # (batch_size, 3, 32, 32), (batch_size)
             pred = model(images)
             # Calculate loss.
             loss = criterion(pred, labels)
@@ -138,7 +177,9 @@ def train_model(device, model, train_loader, val_loader, optimizer, criterion, e
         train_loss_list.append(train_loss)
 
         print()
-        print(f'[{epoch + 1}/{epochs}] {train_time:.2f} sec(s) Train Acc: {train_acc:.5f} | Train Loss: {train_loss:.5f}')
+        print(
+            f"[{epoch + 1}/{epochs}] {train_time:.2f} sec(s) Train Acc: {train_acc:.5f} | Train Loss: {train_loss:.5f}"
+        )
         ##### VALIDATION #####
         model.eval()
         with torch.no_grad():
@@ -146,19 +187,23 @@ def train_model(device, model, train_loader, val_loader, optimizer, criterion, e
             val_loss = 0.0
             val_correct = 0.0
             for batch, data in enumerate(val_loader):
-                sys.stdout.write(f'\r[{epoch + 1}/{epochs}] Validation batch: {batch + 1} / {len(val_loader)}')
+                sys.stdout.write(
+                    f"\r[{epoch + 1}/{epochs}] Validation batch: {batch + 1} / {len(val_loader)}"
+                )
                 sys.stdout.flush()
 
                 # Data loading.
                 images, labels = data
-                images, labels = images.to(device), labels.to(device) # (batch_size, 3, 32, 32), (batch_size)
+                images, labels = images.to(device), labels.to(
+                    device
+                )  # (batch_size, 3, 32, 32), (batch_size)
                 # Forward pass. input: (batch_size, 3, 32, 32), output: (batch_size, 10)
                 pred = model(images)
                 # Calculate loss.
                 loss = criterion(pred, labels)
                 # Evaluate.
                 val_correct += torch.sum(torch.argmax(pred, dim=1) == labels)
-                val_loss += loss.item()            
+                val_loss += loss.item()
 
         # Print validation result
         val_time = time.time() - val_start_time
@@ -168,58 +213,79 @@ def train_model(device, model, train_loader, val_loader, optimizer, criterion, e
         val_loss_list.append(val_loss)
 
         print()
-        print(f'[{epoch + 1}/{epochs}] {val_time:.2f} sec(s) Val Acc: {val_acc:.5f} | Val Loss: {val_loss:.5f}')
-        
+        print(
+            f"[{epoch + 1}/{epochs}] {val_time:.2f} sec(s) Val Acc: {val_acc:.5f} | Val Loss: {val_loss:.5f}"
+        )
+
         # Scheduler step
         # scheduler.step()
-        wandb.log({
-            "epoch": epoch,
-            "train_acc": train_acc,
-            "train_loss": train_loss,
-            "val_acc": val_acc,
-            "val_loss": val_loss
-        })
+        wandb.log(
+            {
+                "epoch": epoch,
+                "train_acc": train_acc,
+                "train_loss": train_loss,
+                "val_acc": val_acc,
+                "val_loss": val_loss,
+            }
+        )
 
         current_result_lists = {
-            'train_acc': train_acc_list,
-            'train_loss': train_loss_list,
-            'val_acc': val_acc_list,
-            'val_loss': val_loss_list
+            "train_acc": train_acc_list,
+            "train_loss": train_loss_list,
+            "val_acc": val_acc_list,
+            "val_loss": val_loss_list,
         }
 
         ##### WRITE LOG #####
         is_better = val_acc >= best_acc
         epoch_time = train_time + val_time
-        write_result_log(os.path.join(logfile_dir, 'result_log.txt'), epoch, epoch_time, train_acc, val_acc, train_loss, val_loss, is_better)
+        write_result_log(
+            os.path.join(logfile_dir, "result_log.txt"),
+            epoch,
+            epoch_time,
+            train_acc,
+            val_acc,
+            train_loss,
+            val_loss,
+            is_better,
+        )
         ##### SAVE THE BEST MODEL #####
         if is_better:
-            print(f'[{epoch + 1}/{epochs}] Save best model to {model_save_dir} ...')
-            torch.save(model.state_dict(), os.path.join(model_save_dir, 'model_best.pth'))
+            print(f"[{epoch + 1}/{epochs}] Save best model to {model_save_dir} ...")
+            torch.save(
+                model.state_dict(), os.path.join(model_save_dir, "model_best.pth")
+            )
             best_acc = val_acc
             counter = 0
         else:
             counter += 1
             if counter >= patience:
-                print(f"Early stopping at epoch {epoch}. Best validation loss: {best_loss:.4f}.")
+                print(
+                    f"Early stopping at epoch {epoch}. Best validation loss: {best_loss:.4f}."
+                )
                 break
-           
+
     plot_learning_curve(logfile_dir, current_result_lists)
 
+
 def test_model(device, model, testloader):
-    model.eval()  
+    model.eval()
     correct = 0
     total = 0
     with torch.no_grad():
         for data in testloader:
             images, labels = data
-            images, labels = images.to(device), labels.to(device) # (batch_size, 3, 32, 32), (batch_size)
+            images, labels = images.to(device), labels.to(
+                device
+            )  # (batch_size, 3, 32, 32), (batch_size)
             # Forward pass. input: (batch_size, 3, 32, 32), output: (batch_size, 10)
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    print(f'acc: {100 * correct / total}%')
+    print(f"acc: {100 * correct / total}%")
+
 
 def write_config(cfg: ModelConfig, config_file_path: str):
     config_dict = {
@@ -229,9 +295,9 @@ def write_config(cfg: ModelConfig, config_file_path: str):
         "Number of Runs": cfg.runs,
         "Epochs": cfg.epochs,
         "Batch Size": cfg.batch_size,
-        "Learning Rate": cfg.learning_rate
+        "Learning Rate": cfg.learning_rate,
     }
-    with open(config_file_path, 'w') as f:
+    with open(config_file_path, "w") as f:
         yaml.dump(config_dict, f)
 
     print(f"Configuration saved to {config_file_path}")
@@ -241,31 +307,31 @@ def main(cfg):
     print(f"Running experiment {cfg.exp_name} with model {cfg.model_name}")
     wandb.init(
         project=cfg.exp_name,
-        config={
-            "learning_rate": cfg.learning_rate,
-            "epoch": cfg.epochs
-        },
+        config={"learning_rate": cfg.learning_rate, "epoch": cfg.epochs},
         name=cfg.model_name,
     )
     # Experiment name
-    exp_name = cfg.model_type + datetime.now().strftime('_%Y_%m_%d_%H_%M_%S') + '_' + cfg.exp_name
-
+    exp_name = (
+        cfg.model_type
+        + datetime.now().strftime("_%Y_%m_%d_%H_%M_%S")
+        + "_"
+        + cfg.exp_name
+    )
 
     # Write log file for config
-    logfile_dir = os.path.join('./experiment', exp_name, 'log')
+    logfile_dir = os.path.join("./experiment", exp_name, "log")
     os.makedirs(logfile_dir, exist_ok=True)
     # Write config
-    config_path = os.path.join('./experiment', exp_name, 'config.ysaml')
+    config_path = os.path.join("./experiment", exp_name, "config.ysaml")
     write_config(cfg, config_path)
     # write_config_log(os.path.join(logfile_dir, 'config_log.txt'))
-    model_save_dir = os.path.join('./experiment', exp_name, 'model')
+    model_save_dir = os.path.join("./experiment", exp_name, "model")
     os.makedirs(model_save_dir, exist_ok=True)
 
     # MODEL
     # (ViT backbone)
     model = timm.create_model(cfg.model_name, pretrained=True, num_classes=10)
     # summary(model, input_size=(1, 3, 224, 224))
-    
 
     # GPU setting
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -275,22 +341,27 @@ def main(cfg):
 
     # DATALOADER
     # Set CIFAR-10 img from to 224x224
-    transform_train = transforms.Compose([
-        transforms.Resize(input_size[1:]),  
-        # transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+    transform_train = transforms.Compose(
+        [
+            transforms.Resize(input_size[1:]),
+            # transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+        ]
+    )
 
-    ])
-
-    transform_test = transforms.Compose([
-        transforms.Resize(input_size[1:]),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
-    ])
+    transform_test = transforms.Compose(
+        [
+            transforms.Resize(input_size[1:]),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+        ]
+    )
 
     # Load CIFAR-10
-    trainset_full = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    trainset_full = torchvision.datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transform_train
+    )
 
     # 70% training, 30% val
     train_size = int(0.7 * len(trainset_full))
@@ -298,34 +369,48 @@ def main(cfg):
     trainset, valset = random_split(trainset_full, [train_size, val_size])
 
     # DataLoader
-    train_loader = DataLoader(trainset, batch_size=cfg.batch_size, shuffle=True, num_workers=4)
-    val_loader = DataLoader(valset, batch_size=cfg.batch_size, shuffle=False, num_workers=4)
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-    test_loader = DataLoader(testset, batch_size=cfg.batch_size, shuffle=False, num_workers=4)
-
+    train_loader = DataLoader(
+        trainset, batch_size=cfg.batch_size, shuffle=True, num_workers=4
+    )
+    val_loader = DataLoader(
+        valset, batch_size=cfg.batch_size, shuffle=False, num_workers=4
+    )
+    testset = torchvision.datasets.CIFAR10(
+        root="./data", train=False, download=True, transform=transform_test
+    )
+    test_loader = DataLoader(
+        testset, batch_size=cfg.batch_size, shuffle=False, num_workers=4
+    )
 
     # OPTIMIZER
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=cfg.learning_rate)
     milestones = [16, 32, 45]
     # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones, gamma=0.1)
-    
-    
+
     # TRAIN
-    train_model(device, model, train_loader, val_loader, optimizer, criterion, cfg.epochs, model_save_dir, logfile_dir)
+    train_model(
+        device,
+        model,
+        train_loader,
+        val_loader,
+        optimizer,
+        criterion,
+        cfg.epochs,
+        model_save_dir,
+        logfile_dir,
+    )
     test_model(device, model, test_loader)
     wandb.finish()
-    
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with open("config.yaml", "r") as file:
         configs = yaml.safe_load(file)
 
     for model_cfg in configs["models"]:
         cfg = ModelConfig(**model_cfg)
         main(cfg)
-
 
     # cfg = ModelConfig(
     #     exp_name = "cait",
